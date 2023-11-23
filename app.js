5a2c70cb96e0816ec143ee96d16cdb46786bbe7d
Mert Önder <132884173+mertonderr@users.noreply.github.com>
2023-11-23 14:07:31 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+function otomatikGecis(sayfaAdi, sure) {
+    setTimeout(function() {
+        window.location.href = sayfaAdi;
+    }, sure);
+}
+
+document.getElementById("mutfakLink").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    otomatikGecis("mutfak.html", 1000); 
+});
+
+function otomatikGecis(sayfaAdi, sure) {
+    setTimeout(function() {
+        window.location.href = sayfaAdi;
+    }, sure);
+}
+
+document.getElementById("banyoLink").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    otomatikGecis("banyo.html", 1000); 
+});
+
+function otomatikGecis(sayfaAdi, sure) {
+    setTimeout(function() {
+        window.location.href = sayfaAdi;
+    }, sure);
+}
+
+document.getElementById("portmantoLink").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    otomatikGecis("portmanto.html", 1000); 
+});
+
+function otomatikGecis(sayfaAdi, sure) {
+    setTimeout(function() {
+        window.location.href = sayfaAdi;
+    }, sure);
+}
+
+document.getElementById("gardropLink").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    otomatikGecis("gardrop.html", 1000); 
+});
+
+function otomatikGecis(sayfaAdi, sure) {
+    setTimeout(function() {
+        window.location.href = sayfaAdi;
+    }, sure);
+}
+
+document.getElementById("kapıLink").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    otomatikGecis("kapı.html", 1000); 
+});
